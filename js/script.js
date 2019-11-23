85540144fac53d665b08dd5d921cb56e5faa85d5
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-11-23 20:14:24 +0000

    added radio buttons to html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let map, places, infoWindow;
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
@@ -16,16 +14,15 @@ function initMap() {
  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
  var locations = [
-   
-    { lat: 40.6331, lng: -89.3985, state},
-    { lat: 37.9643, lng: -91.8318, state},
+   { lat: 40.6331, lng: -89.3985, state},
+   { lat: 37.9643, lng: -91.8318, state},
     { lat: 39.0119, lng: -98.4842, state},
     { lat: 35.0078, lng: -97.0929, state},
-    { lat: 31.9686, lng: -99.9018, state},                             
+    { lat: 31.9686, lng: -99.9018, state},
     { lat: 35.5199, lng: -105.8701, state},
     { lat: 34.0489, lng: -111.0937, state},
     { lat: 36.7783, lng: -119.4179, state},
-
+    
     { lat: 36.0544, lng: -112.1401, nationalPark},
     { lat: 34.9100, lng: -109.8068, nationalPark},
     { lat: 38.5029, lng: -90.5935, nationalPark},
@@ -33,6 +30,7 @@ function initMap() {
     { lat: 34.9003, lng: -107.9942, nationalPark},
     { lat: 34.3238, lng: -115.4728, nationalPark},
     { lat: 35.141689, lng: -115.510399, nationalPark},
+    
     
     { lat: 35.187223, lng: -101.987097, attraction},
     { lat: 34.0103, lng: -118.4962, attraction},
@@ -54,29 +52,6 @@ var markers = locations.map(function(location, i) {
   
   var markerCluster = new MarkerClusterer(map, markers,
             {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-    
-  }
-  
- 
 
- /*
- 
- function fetchAttractionInformation(event) {
-   var attraction = $("#attractions").val();
-   if (!attraction) {
-     $("#attraction-info").html('<h3>Enter National Parks/Trails or attractions</h2>');
-     return;
-   }
- }
- 
- function fetchAttractionInformation(event) {
-   var nationalPark = $("#nationalPark").val();
-   if (!nationalPark) {
-     $("#nationalparks-info").html('<h3>Enter National Parks/Trails or attractions</h2>');
-     return;
-   }
- }
- */
- 
- 
- 
+  
+}
