9495554f0332c77a9cd3039edf338107019bbf35
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-11-24 17:31:08 +0000

    amended some of the id name in html and script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var map;
 
-var z = 5; 
+var z = 6; 
 
 var loc = { lat: 37.0902, lng: -95.7129 };
 
@@ -9,20 +9,17 @@ var search_for = [];
 var locations = {
 
     'arizona': [34.0489, -111.0937],
-
     'california': [36.7783, -119.4179],
-
     'illinois': [40.6331, -89.3985],
-
     'kansas': [39.0119, -98.4842],
-
     'missouri': [37.9643, -91.8318],
-
     'newmexico': [31.5199, -105.8701],
-
     'oklahoma': [35.0078, -97.0929],
-
     'texas': [31.9686, -99.9018],
+    
+    'park': [36.0544,  -112.1401],
+    'parkone': [34.9100, -109.8068],
+   
 
 };
 
@@ -33,22 +30,19 @@ function initMap() {
 }
 
 
-var city = document.getElementById("city");
+var state = document.getElementById("state");
 
-function citymap(city, zoom) {
+function citymap(state, zoom) {
 
-map.setCenter(new google.maps.LatLng(locations[city.value][0], locations[city.value][1]));
+map.setCenter(new google.maps.LatLng(locations[state.value][0], locations[state.value][1]));
  map.setZoom(zoom);
 
 }
 
-city.addEventListener("change", function() {
-
-    citymap(city, z);
-
-    $('input[type="radio"]').prop('checked', false);   
-
-    $("#places").empty();   
+state.addEventListener("change", function() {
+  citymap(state, z);
+  $('input[type="radio"]').prop('checked', false);   
+  $("#places").empty();   
 
 });
 
