0ccfd5aec2062ededa840d0939ea094a4e2175c2
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-11-28 20:02:45 +0000

    temporarily blocked out my code and copy and pasted google maps api because my places search won't work and i am commiting this to send to the code institute for help

--- js/script.js
+++ js/script.js
@@ -1,57 +1,87 @@
-var map;
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+        var card = document.getElementById('pac-card');
+        var input = document.getElementById('pac-input');
+        var types = document.getElementById('type-selector');
+        var strictBounds = document.getElementById('strict-bounds-selector');
 
-var google;
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
-var z = 6; 
+        var autocomplete = new google.maps.places.Autocomplete(input);
 
-var loc = { lat: 37.0902, lng: -95.7129 };
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
 
-function initMap(state, zoom) {
-    map = new google.maps.Map(document.getElementById("map",), { center: loc, zoom: z }); 
-   
+        // Set the data fields to return when the user selects a place.
+        autocomplete.setFields(
+            ['address_components', 'geometry', 'icon', 'name']);
 
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
 
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var states = [
-    
-    { lat: 40.6331, lng: -89.3985},
-    { lat: 37.9643, lng: -91.8318},
-    { lat: 39.0119, lng: -98.4842},
-    { lat: 35.0078, lng: -97.0929},
-    { lat: 31.9686, lng: -99.9018},
-    { lat: 35.5199, lng: -105.8701},
-    { lat: 34.0489, lng: -111.0937},
-    { lat: 36.7783, lng: -119.4179},
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
 
-    
-    ];
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
 
-    
-var markers = states.map(function(location, i) {
-    return new google.maps.Marker({
-    position: location, 
-    label: labels[i % labels.length]
-      });
-    });
-  
-  var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-  
-}
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
 
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
 
-var state = document.getElementById("state");
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
 
-function citymap(states, zoom) {
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
 
-map.setCenter(new google.maps.LatLng(states[state.value][0], states[state.value][1]));
- map.setZoom(zoom);
-
-}
-
-state.addEventListener("change", function() {
-  citymap(state, z);
-  $('input[type="radio"]').prop('checked', false);   
-  $("#places").empty();   
-
-});
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+      }
--- practice.js
+++ practice.js
@@ -53,4 +53,62 @@ state.addEventListener("change", function() {
 
 
 
+var map;
+
+var google;
+
+var z = 6; 
+
+var loc = { lat: 37.0902, lng: -95.7129 };
+
+function initMap(state, zoom) {
+    map = new google.maps.Map(document.getElementById("map",), { center: loc, zoom: z }); 
+   
+
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var states = [
+    
+    { lat: 40.6331, lng: -89.3985},
+    { lat: 37.9643, lng: -91.8318},
+    { lat: 39.0119, lng: -98.4842},
+    { lat: 35.0078, lng: -97.0929},
+    { lat: 31.9686, lng: -99.9018},
+    { lat: 35.5199, lng: -105.8701},
+    { lat: 34.0489, lng: -111.0937},
+    { lat: 36.7783, lng: -119.4179},
+
+    
+    ];
+
+    
+var markers = states.map(function(location, i) {
+    return new google.maps.Marker({
+    position: location, 
+    label: labels[i % labels.length]
+      });
+    });
+  
+  var markerCluster = new MarkerClusterer(map, markers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+  
+}
+
+
+var state = document.getElementById("state");
+
+function citymap(states, zoom) {
+
+map.setCenter(new google.maps.LatLng(states[state.value][0], states[state.value][1]));
+ map.setZoom(zoom);
+
+}
+
+state.addEventListener("change", function() {
+  citymap(state, z);
+  $('input[type="radio"]').prop('checked', false);   
+  $("#places").empty();   
+
+});
+
 
