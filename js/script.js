2ce2beb4c552ea34062760e58f88f982b107c0f8
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-12-07 16:34:30 +0000

    changed the content of html and amended alot of css

--- js/script.js
+++ js/script.js
@@ -1,34 +1,31 @@
+//I used Google Maps API documentation and took aspects of 'hotel autosearch' to create my map
 
-//Global Variables---------------------------------
 var map;
 var infoWindow;
 var placesService;
 var markers = [];
-var america = {lat: 37.0902, lng: -95.7129};
+var america = { lat: 37.0902, lng: -95.7129 };
 var autocomplete;
 var placeType;
+var image = 'https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1280px-Flag_of_the_United_States.svg.png';
 
 
-//Initialize Map------------------------------------
+//Generate map ------------------------------------- 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: america,
     zoom: 4
   });
-  
-  // Info window with place details
-  infoWindow = new google.maps.InfoWindow({
-    content: document.getElementById('info-content')
-  });
 
 
+  // Set bounds so that the user can only search for cities in the USA
   autocomplete = new google.maps.places.Autocomplete(
     (
       document.getElementById('searchPlace')), {
       types: ['(cities)'],
-      componentRestrictions: {country: 'us'}
+      componentRestrictions: { country: 'us' }
     });
-    
+
 
   autocomplete.addListener('place_changed', onPlaceChanged);
 
@@ -43,80 +40,83 @@ function initMap() {
       document.getElementById('searchPlace').placeholder = 'Where are you going?';
     }
   }
-  //Nearby place search by click---------------------------
+
   placesService = new google.maps.places.PlacesService(map);
 
 
+  //Jquery learned from the Code Institute lessons to only generate specific markers when the user clicks on the button
 
-
-  //Nearby place search by click---------------------------
-  placesService = new google.maps.places.PlacesService(map);
-  
   document.getElementById("accommodation").addEventListener("click", function() {
     placeType = ['lodging', 'hotel', 'motel'];
     clearMarkers();
     searchPlaces();
   });
   document.getElementById("nationalParks").addEventListener("click", function() {
-    placeType = ['parks', 'hikes', 'national_park', 'canyon_park', 'mountain', 'state_park'];
+    placeType = ['hikes', 'national', 'canyon', 'mountain', 'state_park', 'peak', 'preserve'];
     clearMarkers();
     searchPlaces();
   });
   document.getElementById("attraction").addEventListener("click", function() {
-    placeType = ['night_club', 'zoo', 'museum', 'cinema' ];
+    placeType = ['night_club', 'zoo', 'museum', 'cinema', 'bridge'];
     clearMarkers();
     searchPlaces();
   });
 
 
+
+  //Search places---------------------------------------------------------
+  function searchPlaces() {
+    var search = {
+      bounds: map.getBounds(),
+      types: placeType
+    };
+    placesService.nearbySearch(search, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        //Clear markers---------------------------------------------------
+        clearMarkers();
+        //Create markers----------------------------------------------------
+        for (var i = 0; i < results.length; i++) {
+          markers[i] = new google.maps.Marker({
+            position: results[i].geometry.location,
+            animation: google.maps.Animation.DROP,
+           
+          });
+          markers[i].placeResult = results[i];
+          google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+          setTimeout(dropMarker(i), i * 100);
+        }
+
+      }
+    });
+  }
+
+  //Adding markers
+  function dropMarker(i) {
+    return function() {
+      markers[i].setMap(map);
+      
+    };
+  }
+
+  //Clearing the markers when the user clicks on something else
+  function clearMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      if (markers[i]) {
+        markers[i].setMap(null);
+      }
+    }
+    markers = [];
+  }
+
   //Sets the info window in the place details html element------------------------------
   infoWindow = new google.maps.InfoWindow({
-    content: document.getElementById('info-content')
+    content: document.getElementById('info-box')
   });
-}
-//Search places---------------------------------------------------------
-function searchPlaces() {
-  var search = {
-    bounds: map.getBounds(),
-    types: placeType
-  };
-  placesService.nearbySearch(search, function(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      //Clear markers---------------------------------------------------
-      clearMarkers();
-      //Create markers----------------------------------------------------
-      for (var i = 0; i < results.length; i++) {
-        markers[i] = new google.maps.Marker({
-          position: results[i].geometry.location,
-          animation: google.maps.Animation.DROP
-        });
-        markers[i].placeResult = results[i];
-        google.maps.event.addListener(markers[i], 'click', showInfoWindow); //Show info window when user clicks on a marker---
-        //google.maps.event.addListener(markers[i], 'click', showInfoWindow); Show info window when user clicks on a marker---
-        setTimeout(dropMarker(i), i * 100);
-      }
-
-    }
-  });
-}
-//Add markers to map----------------------------
-function dropMarker(i) {
-  return function() {
-    markers[i].setMap(map);
-  };
-}
-
-//Clear markers---------------------------------
-function clearMarkers() {
-  for (var i = 0; i < markers.length; i++) {
-    if (markers[i]) {
-      markers[i].setMap(null);
-    }
-  }
-  markers = [];
 }
 // Get the place details for a place. Show the information in an info window,
 // anchored on the marker for the place that the user selected.
+
+
 function showInfoWindow() {
   var marker = this;
   placesService.getDetails({ placeId: marker.placeResult.place_id },
@@ -128,5 +128,17 @@ function showInfoWindow() {
       renderPlaceDetails(place);
     });
 }
-    
-   
+
+// Load the place information into from html
+function renderPlaceDetails(place) {
+  document.getElementById('place-name').textContent = place.name;
+  document.getElementById('place-address').textContent = place.formatted_address;
+
+  if (place.formatted_phone_number) {
+    document.getElementById('place-phoneNumber').textContent = place.formatted_phone_number;
+  }
+  else {
+    document.getElementById('place-phoneNumber').textContent = 'Not available';
+  }
+ 
+}
