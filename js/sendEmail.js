ee87b6ae8f2ad89852fe7cbb212f850147bc3533
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-12-14 16:05:49 +0000

    setting up my sendMail.js and adding bootstrap modal to have an alert message

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,22 +1,25 @@
+    function sendMail(contactForm) {
+
+        emailjs.init("user_a9Tx2qqs0p8VMBtOupmUT");
+
+        emailjs.send("gmail", "travelusa", {
+                "from_name": contactForm.name.value,
+                "from_email": contactForm.email.value,
+                "project_request": contactForm.description.value
+            })
+            .then(
+                function(response) {
+                    console.log("SUCCESS", response);
+                    if (confirm('Successful Message')) {
+                        window.location.reload();
+                    }
+
+                },
+
+                function(error) {
+                    console.log("FAILED", error);
+                }
+            );
+        return false;
+    }
     
-function sendMail(contactForm)
-{
-    
-emailjs.init("user_a9Tx2qqs0p8VMBtOupmUT");
- 
-  emailjs.send("gmail","travelusa", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.query.value
-    })
-    .then (
-        function(response) {
-            console.log("SUCCESS", response);   
-        },
-       
-            function(error) {
-                console.log("FAILED", error);
-            }
-        );
-    return false; 
-}
