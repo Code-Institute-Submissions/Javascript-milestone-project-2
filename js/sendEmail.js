34fb92cc94ac8f25cab137d7bd29398c4aa7f852
EC2 Default User <ec2-user@ip-172-31-95-133.ec2.internal>
2019-12-14 12:51:25 +0000

    making amendments to sendEmail.js to try and get the function to work

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,14 +1,22 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail","travelusa", {
+    
+function sendMail(contactForm)
+{
+    
+emailjs.init("user_a9Tx2qqs0p8VMBtOupmUT");
+ 
+  emailjs.send("gmail","travelusa", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "query": contactForm.query.value
+        "project_request": contactForm.query.value
     })
     .then (
         function(response) {
             console.log("SUCCESS", response);   
         },
-        function(error) {
-            console.log("FAILED", error);
-        })
+       
+            function(error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; 
 }
